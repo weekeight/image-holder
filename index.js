44137066ec43636132a4509c2d04a8e41b5879eb
陈锋 <chenf@shinemo.com>
2016-02-20 17:21:57 +0800

    根据url参数生成svg图片完成

--- index.js
+++ index.js
@@ -10,23 +10,68 @@ let port = process.env.PORT || 3000;
 server.listen(port);
 console.log('Server started! It\'s listening on port %s', port);
 
-app.use('/', function(req, res){
-	let reqPath = req.path;
-	console.log(reqPath);
-	let width = reqPath.split('x')[0];
-	let height = reqPath.split('x')[1];
+function createSvgXml({width = 100, height = 100, bgColor = '#999', fColor = '#fff', text = undefined}){
+	if(text){
 
-	if(!width || !height){
-		res.send('参数不正确...');
-		return;
+		return `
+			<svg xmlns="http://www.w3.org/2000/svg" height="${height}" width="${width}">
+				<rect width="${width}" height="${height}" style="fill:${bgColor}" />
+				<text x="50%" y="50%" font-size="20" dy=".3em" fill="${fColor}" text-anchor="middle">${text}</text>
+			</svg>
+		`;
+	}else{
+		return `
+			<svg xmlns="http://www.w3.org/2000/svg" height="${height}" width="${width}">
+				<rect width="${width}" height="${height}" style="fill:${bgColor}" />
+				<text x="50%" y="50%" font-size="20" dy=".3em" fill="${fColor}" text-anchor="middle">${width}x${height}</text>
+			</svg>
+		`;
+	}
+	
+}
+
+app.get('/', function(req, res){
+	res.send('Welcome to image-holder...');
+});
+
+app.get('/:widthHeight', function(req, res, next){
+	let width = req.params.widthHeight.split('x')[0];
+	let height = req.params.widthHeight.split('x')[1];
+
+	if(width && height){
+		var svgXml = createSvgXml({
+			width: width,
+			height: height
+		});
+		res.type('image/svg+xml');
+		res.send(svgXml);
+		res.end();
+	}else{
+		next();
 	}
 
+});
+
+
+app.get('/:widthHeight/:bgColor/:fColor', function(req, res){
+	let width = req.params.widthHeight.split('x')[0];
+	let height = req.params.widthHeight.split('x')[1];
+	let bgColor = req.params.bgColor;
+	let fColor = req.params.fColor;
+	let text = req.query.text;
+
+	let svgXml = createSvgXml({
+		width: width,
+		height: height,
+		bgColor: '#' + bgColor,
+		fColor: '#' + fColor,
+		text: text
+	});
+
 	res.type('image/svg+xml');
-	res.send(`
-		<svg height="200" width="200">
-			<rect width="200" height="200" style="fill:#999" />
-			<text x="50%" y="50%" font-size="20" dy=".3em" fill="#fff" text-anchor="middle">200x200</text>
-		</svg>
-	`);
+	res.send(svgXml);
 	res.end();
 });
+
+app.use((req, res) => {res.send('参数错误，请检查参数后重试...');});
+
